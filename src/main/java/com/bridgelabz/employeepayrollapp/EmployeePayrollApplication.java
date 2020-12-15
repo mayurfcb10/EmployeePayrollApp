72808c9c6080c996e9da6eb960826124cb167eb1
Mayur <mayurzope555@gmail.com>
2020-12-15 14:56:34 +0530

    [Mayur]Added: DataBase Setting done as environment variables

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollApplication.java
@@ -13,6 +13,7 @@ public class EmployeePayrollApplication {
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(EmployeePayrollApplication.class, args);
 		log.info("Employee Payroll App started in {} Environment",context.getEnvironment().getProperty("environment"));
+		log.info("Employee Payroll DB user is {}",context.getEnvironment().getProperty("spring.datasource.username"));
 	}
 
 }
